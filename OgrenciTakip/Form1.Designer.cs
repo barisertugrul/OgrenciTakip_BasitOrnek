a6e61ee187fd551edbb000466fb376fb6714cc85
Barış ERTUĞRUL <barisertugrul@ertyazilim.com>
2021-04-20 12:05:26 +0300

    Proje dosyası ekle.

---
+++ OgrenciTakip/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿
+namespace OgrenciTakip
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtAra = new System.Windows.Forms.TextBox();
+            this.dgvKisiselBilgi = new System.Windows.Forms.DataGridView();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.txtAdi = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtSoyadi = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.btnGuncelle = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvKisiselBilgi)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(288, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(170, 28);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Veritabanı Bilgileri";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 28);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ara";
+            // 
+            // txtAra
+            // 
+            this.txtAra.Location = new System.Drawing.Point(65, 61);
+            this.txtAra.Name = "txtAra";
+            this.txtAra.Size = new System.Drawing.Size(723, 34);
+            this.txtAra.TabIndex = 2;
+            this.txtAra.TextChanged += new System.EventHandler(this.txtAra_TextChanged);
+            // 
+            // dgvKisiselBilgi
+            // 
+            this.dgvKisiselBilgi.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvKisiselBilgi.Location = new System.Drawing.Point(17, 112);
+            this.dgvKisiselBilgi.Name = "dgvKisiselBilgi";
+            this.dgvKisiselBilgi.RowHeadersWidth = 51;
+            this.dgvKisiselBilgi.RowTemplate.Height = 29;
+            this.dgvKisiselBilgi.Size = new System.Drawing.Size(471, 319);
+            this.dgvKisiselBilgi.TabIndex = 3;
+            this.dgvKisiselBilgi.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvKisiselBilgi_CellClick);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(512, 121);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 28);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Id";
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(589, 118);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(75, 34);
+            this.txtId.TabIndex = 5;
+            // 
+            // txtAdi
+            // 
+            this.txtAdi.Location = new System.Drawing.Point(589, 158);
+            this.txtAdi.Name = "txtAdi";
+            this.txtAdi.Size = new System.Drawing.Size(199, 34);
+            this.txtAdi.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(512, 161);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 28);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Adı";
+            // 
+            // txtSoyadi
+            // 
+            this.txtSoyadi.Location = new System.Drawing.Point(589, 198);
+            this.txtSoyadi.Name = "txtSoyadi";
+            this.txtSoyadi.Size = new System.Drawing.Size(199, 34);
+            this.txtSoyadi.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(512, 201);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(72, 28);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Soyadı";
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Location = new System.Drawing.Point(512, 269);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(276, 50);
+            this.btnEkle.TabIndex = 10;
+            this.btnEkle.Text = "Verileri Ekle";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // btnSil
+            // 
+            this.btnSil.Location = new System.Drawing.Point(512, 325);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(276, 50);
+            this.btnSil.TabIndex = 11;
+            this.btnSil.Text = "Veriyi Sil";
+            this.btnSil.UseVisualStyleBackColor = true;
+            this.btnSil.Click += new System.EventHandler(this.btnSil_Click);
+            // 
+            // btnGuncelle
+            // 
+            this.btnGuncelle.Location = new System.Drawing.Point(512, 381);
+            this.btnGuncelle.Name = "btnGuncelle";
+            this.btnGuncelle.Size = new System.Drawing.Size(276, 50);
+            this.btnGuncelle.TabIndex = 12;
+            this.btnGuncelle.Text = "Veriyi Güncelle";
+            this.btnGuncelle.UseVisualStyleBackColor = true;
+            this.btnGuncelle.Click += new System.EventHandler(this.btnGuncelle_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 28F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnGuncelle);
+            this.Controls.Add(this.btnSil);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.txtSoyadi);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtAdi);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.dgvKisiselBilgi);
+            this.Controls.Add(this.txtAra);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Name = "Form1";
+            this.Text = "Öğrenci Bilgileri";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvKisiselBilgi)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtAra;
+        private System.Windows.Forms.DataGridView dgvKisiselBilgi;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.TextBox txtAdi;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtSoyadi;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.Button btnGuncelle;
+    }
+}
+
---
+++ OgrenciTakip/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Markup;
+using Microsoft.Data.SqlClient;
+
+namespace OgrenciTakip
+{
+    public partial class Form1 : Form
+    {
+
+        private SqlConnection baglanti =
+            new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=OgrenciDB;Integrated Security=True");
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void GridListele(string filter="")
+        {
+            SqlCommand sorgu;
+            if (filter != "")
+            {
+                sorgu = new SqlCommand("Select * From OgrenciBilgileri Where Adi like @adi",baglanti);
+                sorgu.Parameters.AddWithValue("@adi", "%" + filter + "%");
+            }
+            else
+            {
+                sorgu = new SqlCommand("Select * From OgrenciBilgileri", baglanti);
+            }
+
+            SqlDataAdapter data = new SqlDataAdapter(sorgu);
+            DataSet dataSet = new DataSet();
+            baglanti.Open();
+            data.Fill(dataSet,"OgrenciBilgileri");
+            dgvKisiselBilgi.DataSource = dataSet.Tables["OgrenciBilgileri"];
+            txtId.Text = "";
+            txtAdi.Text = "";
+            txtSoyadi.Text = "";
+            baglanti.Close();
+        }
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            SqlCommand eklemeSorgusu = new SqlCommand(
+                "insert into OgrenciBilgileri (Adi,Soyadi) values (@adi,@soyadi)",baglanti);
+            eklemeSorgusu.Parameters.AddWithValue("@adi", txtAdi.Text);
+            eklemeSorgusu.Parameters.AddWithValue("@soyadi", txtSoyadi.Text);
+            eklemeSorgusu.ExecuteNonQuery();
+            MessageBox.Show("Öğrenci bilgileri veritabanına kaydedildi", "Bilgi", MessageBoxButtons.OK,
+                MessageBoxIcon.Information);
+            baglanti.Close();
+            GridListele();
+        }
+
+        private void dgvKisiselBilgi_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            txtId.Text = dgvKisiselBilgi.Rows[e.RowIndex].Cells[0].Value.ToString();
+            txtAdi.Text = dgvKisiselBilgi.Rows[e.RowIndex].Cells[1].Value.ToString();
+            txtSoyadi.Text = dgvKisiselBilgi.Rows[e.RowIndex].Cells[2].Value.ToString();
+        }
+
+        private void btnSil_Click(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            SqlCommand silmeSorgusu = new SqlCommand("Delete from OgrenciBilgileri where Id=@id", baglanti);
+            silmeSorgusu.Parameters.AddWithValue("@id", txtId.Text);
+            var yanit = MessageBox.Show("Öğrenci bilgilerini silmek istediğinize emin misiniz?", "Uyarı",
+                MessageBoxButtons.YesNoCancel, MessageBoxIcon.Warning);
+            if (yanit == DialogResult.Yes)
+            {
+                silmeSorgusu.ExecuteNonQuery();
+                MessageBox.Show("Öğrenci bilgileri veritabanından silindi", "Bilgi", MessageBoxButtons.OK,
+                    MessageBoxIcon.Information);
+            }
+            
+            baglanti.Close();
+            GridListele();
+        }
+
+        private void btnGuncelle_Click(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            SqlCommand guncellemeSorgusu =
+                new SqlCommand("Update OgrenciBilgileri set Adi=@adi, Soyadi=@soyadi where Id=@id", baglanti);
+            guncellemeSorgusu.Parameters.AddWithValue("@adi", txtAdi.Text);
+            guncellemeSorgusu.Parameters.AddWithValue("@soyadi", txtSoyadi.Text);
+            guncellemeSorgusu.Parameters.AddWithValue("@id", txtId.Text);
+            baglanti.Close();
+            MessageBox.Show("Öğrenci bilgileri güncellendi", "Bilgi", MessageBoxButtons.OK,
+                MessageBoxIcon.Information);
+            GridListele();
+        }
+
+        private void txtAra_TextChanged(object sender, EventArgs e)
+        {
+            GridListele(txtAra.Text);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            GridListele();
+        }
+    }
+}
